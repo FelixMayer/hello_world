03aa3bd1ff2076ce74f55f0ceeb939f027f637e1
Felix <felix-mayer@gmx.de>
2021-04-08 18:05:25 +0200

    first commit

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+name = "Felix"
+print("Hello", name)
+
+print("Hello" + name)
+
+favNum = 1
+
+print("Hello", favNum)
+
+print("Hello" + str(favNum))
+
+favfood1 = "Sushi"
+favfood2 = "Ramen"
+
+print("I love to eat {} and {}.".format(favfood1, favfood2))
+
+print(f"I love to eat {favfood1} and {favfood2}.")
+
